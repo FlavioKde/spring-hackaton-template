cbbd729d5feb6bf0261d709006adb1a7104865d5
FlavioKde <flaviodavirro@gmail.com>
2026-05-08 17:44:33 +0200

    feat(HealthController): add new annotation

--- src/main/java/com/interhack/spring_hackaton_template/controller/HealthController.java
+++ src/main/java/com/interhack/spring_hackaton_template/controller/HealthController.java
@@ -1,9 +1,11 @@
 package com.interhack.spring_hackaton_template.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     @GetMapping("/health")
     public String health() {
