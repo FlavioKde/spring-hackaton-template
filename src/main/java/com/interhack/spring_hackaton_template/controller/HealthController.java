886811d2c3507e06a3bd44bc3d05925617619d7d
FlavioKde <flaviodavirro@gmail.com>
2026-05-08 17:06:14 +0200

    feat: add basic health endpoint and project structure

---
+++ src/main/java/com/interhack/spring_hackaton_template/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.interhack.spring_hackaton_template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "Hello World";
+    }
+}
