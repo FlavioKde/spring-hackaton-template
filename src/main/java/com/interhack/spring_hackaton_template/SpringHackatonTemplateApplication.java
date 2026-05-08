eac7964a4321d867feab2f405c1e02c7b65b6688
FlavioKde <flaviodavirro@gmail.com>
2026-05-08 15:32:12 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/interhack/spring_hackaton_template/SpringHackatonTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.interhack.spring_hackaton_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHackatonTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHackatonTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interhack/spring_hackaton_template/SpringHackatonTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interhack.spring_hackaton_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHackatonTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
